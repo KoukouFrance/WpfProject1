3a752154030383c2f1166cf4e2282c7de2fad70b
Karim Chakib <kchakib99@gmail.com>
2017-05-13 07:25:48 +0200

    Add Metrics property to Commands

--- WpfAppTest1/Commands/CustomerUpdateCommand.cs
+++ WpfAppTest1/Commands/CustomerUpdateCommand.cs
@@ -7,7 +7,10 @@ namespace WpfAppTest1.Commands
 {
     internal class CustomerUpdateCommand : ICommand
     {
+        private double metrics;
         private CustomerViewModel _customerViewModel;
+
+        public double Metrics  { get; set; }
 
         public CustomerUpdateCommand(CustomerViewModel customerViewModel)
         {
